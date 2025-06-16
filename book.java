39eae61c9078e655dfac25b681b02694e58ef0ca
NIYATI986 <niyatibalayan27@gmail.com>
2025-06-17 05:04:42 +0530

    Initial commit

---
+++ book.java
@@ -0,0 +1,40 @@
+public class book{
+    private String id;
+    private String title;
+    private String author;
+    private boolean isIssued;
+
+    //Constructors
+    public book(String id, String title, String author, boolean isIssued){
+        this.id=id;
+        this.title=title;
+        this.author=author;
+        this.isIssued=false;// it means by default , the book isn't issued.
+    }
+
+    //Getters
+    public String getId(){
+        return id;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getAuthor(){
+        return author;
+    }
+    public boolean isIssued(){
+        return isIssued;
+    }
+    //Issue the book
+    public void issueBook(){
+        isIssued=true;
+    }
+    //Return the book
+    public void returnBook(){
+        isIssued=false;
+    }
+    //Book details
+    public String toString(){
+        return "Id:" + id + " , Title : " + title + " , Author : " + author + " , Issued : " + isIssued;
+    }
+}
